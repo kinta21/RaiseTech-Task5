96e40042a3a985823b68a58c060d4424e4cd48da
kinta21 <kinta21127@yahoo.ne.jp>
2023-08-13 15:08:17 +0900

    プロジェクトをセットアップ

---
+++ src/CarShop.java
@@ -0,0 +1,17 @@
+public class CarShop {
+    private String name;
+    private int founding;
+
+    public CarShop(String name, int founding) {
+        this.name = name;
+        this.founding = founding;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFounding() {
+        return founding;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        CarShop mazda = new CarShop("mazda", 1920);
+        CarShop suzuki = new CarShop("suzuki", 1909);
+        CarShop toyota = new CarShop("toyota", 1937);
+        CarShop honda = new CarShop("honda", 1948);
+
+        List<CarShop> carShops = List.of(mazda, suzuki, toyota, honda);
+
+        System.out.println("<創業者の苗字つく日本車メーカー>");
+
+        for (CarShop shop : carShops) {
+            System.out.println(shop.getName());
+            System.out.println(shop.getFounding() + "年創業");
+        }
+
+        System.out.println("<創業100年以上でzのつく自動車メーカー>");
+
+        carShops.stream()
+                .filter(carShop -> carShop.getFounding() <= 1923)
+                .filter(carShop -> carShop.getName().contains("z"))
+                .map(carShop -> carShop.getName().toUpperCase())
+                .forEach(System.out::println);
+
+    }
+}
